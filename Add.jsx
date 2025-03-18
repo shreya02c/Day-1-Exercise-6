ec7706d6b251b3bbf9975965d44dada8963c424c
shreya02c <shreyachaudhari996@gmail.com>
2025-03-18 15:19:24 +0530

    Add files via upload

---
+++ Add.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+    return (
+        <div>
+            <p data-testid="price">$100</p>
+            <button>Apply Discount</button>
+        </div>
+    );
+}
